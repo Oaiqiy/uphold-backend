f74af7e9cd5479df8bd0ca46e64ca6e5071c9c58
Oaiqiy <1320371940@qq.com>
2022-05-03 21:14:50 +0800

    fix: user collect a gym twice throw exception.

--- src/main/java/dev/oaiqiy/uphold/api/user/UserCollectionController.java
+++ src/main/java/dev/oaiqiy/uphold/api/user/UserCollectionController.java
@@ -6,6 +6,7 @@ import dev.oaiqiy.uphold.data.UserRepo;
 import dev.oaiqiy.uphold.domain.Gym;
 import dev.oaiqiy.uphold.domain.User;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user/collection")
+@RequestMapping("/user/collect")
 @AllArgsConstructor
+@Slf4j
 public class UserCollectionController {
     private final UserRepo userRepo;
     private final GymRepo gymRepo;
@@ -27,9 +29,14 @@ public class UserCollectionController {
         if(gym == null)
             return new ResultInfo<>(0,"no such gym");
 
+        if(user.getCollection().stream().anyMatch(g -> g.getId().equals(id)))
+            return ResultInfo.failure();
+
         user.getCollection().add(gym);
+
         userRepo.save(user);
 
+        log.info("user: " + user.getPhone() + " collect " + id);
         return ResultInfo.success();
 
     }
@@ -42,8 +49,11 @@ public class UserCollectionController {
         if(gym == null)
             return new ResultInfo<>(0,"no such gym");
 
-        user.getCollection().remove(gym);
 
+        user.getCollection().removeIf(g -> g.getId().equals(id));
+        userRepo.save(user);
+
+        log.info("user: " + user.getPhone() + "cancel collect " + id);
         return ResultInfo.success();
     }
 }
